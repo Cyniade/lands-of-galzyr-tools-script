51a38a2792b7f94184c6e6a13e3bc5340279a6a1
Cyniade <lucas.noffray@gmail.com>
2022-12-24 15:28:38 +0100

    origin verbs visibility
    Only show origin verbs when translation is active.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lands of Galzyr - Tools
 // @namespace    https://stories.daimyria.fi
-// @version      0.1
+// @version      0.2
 // @description  Provide tools for Lands of Galzyr PWA.
 // @author       https://github.com/Cyniade
 // @match        https://stories.daimyria.fi
@@ -11,8 +11,21 @@
 (function() {
     'use strict';
     document.addEventListener("click", () => preventVerbsTranslation());
+    var observer = new MutationObserver(function (event) {
+        if(!document.documentElement.lang.match('en-GB|auto')) {
+            originVerbVisibility(false);
+        } else {
+            originVerbVisibility(true);
+        }
+    });
+    
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['class'],
+      childList: false,
+      characterData: false
+    });
 })();
-
 
 /**
  * Keep original text from verb next to the translated one.
@@ -25,8 +38,22 @@ function preventVerbsTranslation() {
             verbElems[i].classList.add('processed');
             let originVerb = verbElems[i].cloneNode(true);
             originVerb.classList.add('notranslate');
+            originVerb.classList.add('origin-verb');
             originVerb.textContent = ' {' + originVerb.textContent + '} ';
+            originVerb.hidden = true;
             verbElems[i].before(originVerb);
         }
+    }
+}
+
+/**
+ * Hide or show origin verbs on page.
+ * 
+ * @param {bool} hidden
+ */
+function originVerbVisibility(hidden) {
+    let originVerbs = document.getElementsByClassName('origin-verb');
+    for (var i = 0; i < originVerbs.length; i++) {
+        originVerbs[i].hidden = hidden;
     }
 }
